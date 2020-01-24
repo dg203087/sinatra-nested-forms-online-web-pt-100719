c37b6984a23ae04a11b486d2f1f873a52805c9b9
Deanna Kelley <deanna.georges@gmail.com>
2020-01-24 18:27:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,9 @@ module FormsLab
     end
     
     post '/pirates' do
-      
-      @pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height],  weight: [:pirate][:weight])
       binding.pry
+      @pirate = Pirate.new(name: params[:pirate][:name], height: params[:pirate][:height],  weight: [:pirate][:weight])
+    
       ships = params[:pirate][:ships]
       ships.each do |ship_details|
           Ship.new({name: ship_details[:name], type: ship_details[:type], booty: ship_details[:booty]})
